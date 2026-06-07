09cb113538b661073f722947797d4ce3d1c9f239
fer1231231 <alwaysalert.12312@gmail.com>
2026-06-07 22:04:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const API_KEY = process.env.GEMINI_API_KEY;
-const GEMINI_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest
+const GEMINI_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash
 :generateContent?key=${API_KEY}`;
 
 app.use(cors());
